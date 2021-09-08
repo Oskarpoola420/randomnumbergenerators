0989ab308cf3ff0dca9f4cc5f9dd56c972b13f3a
Opilane TTHK <opilane@tthk.ee>
2021-09-08 12:30:56 +0300

    random

---
+++ EpicDiceGame/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EpicDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string PlayerOne = "joe";
+            string PlayerTwo = "mama";
+
+            int joeThrow = rnd.Next(1, 7);
+            int mamathrow = rnd.Next(1, 7);
+
+            Console.WriteLine($"{PlayerOne} threw {joeThrow};");
+            Console.WriteLine($"{PlayerTwo} threw {mamathrow};");
+            if(joeThrow > mamathrow)
+            {
+                Console.WriteLine($"{PlayerOne} Wins!");
+            }
+            else if (joeThrow < mamathrow)
+            {
+                Console.WriteLine($"{PlayerTwo} Wins!");
+            }
+            else
+            {
+                Console.WriteLine("draw! let them try again.");
+            }
+        }
+    }
+}
---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int MyRandomNuber = rnd.Next(1, 11);
+            //programm genereerib juhuslikku numbrit 1 kuni 10
+            //programm kontrollib kas number on suurem kui 5
+            // kui number on suure, kui 5, siis konsool kuvab
+            //juhuslik number on {myrandomnumber} see on suurem kui 5
+            // kui number on väiksem kui 5 konsool kuvab
+
+
+            Console.WriteLine($"arvuti genereeris: {MyRandomNuber}");
+            if(MyRandomNuber > 5)
+            {
+                Console.WriteLine($"juhuslik numbe on {MyRandomNuber}, see on suurem, kui 5");
+            }
+            else if (MyRandomNuber < 5)
+            {
+                Console.WriteLine($"juhuslik numbe on {MyRandomNuber}, see on väiksem, kui 5");
+            }
+            else
+            {
+                Console.WriteLine("juhuslik number ongi 5");
+            }
+        }
+    }
+}
